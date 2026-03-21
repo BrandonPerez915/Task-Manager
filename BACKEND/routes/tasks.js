4b1944e426dde3a646bf4e4ede2c5dd45427b0dc
Brandon Pérez <brandonperez@Brandons-Mac-mini.local>
2026-03-21 00:28:58 -0600

    added task routes to router

--- BACKEND/routes/tasks.js
+++ BACKEND/routes/tasks.js
@@ -1,7 +1,21 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { Router } from 'express'
 
-const tasksRouter = express.Router
+import { users, tasks } from '../database/storage.js'
+
+import { matchUserPassword, requireTask, requireTaskProps } from './middlewares.js'
+import * as tasksController from '../controllers/tasks_controller.js'
+
+const tasksRouter = Router()
+const matchUser = matchUserPassword(users)
+const requiredTask = requireTask(tasks)
+
+tasksRouter.route('/')
+  .post(matchUser, tasksController.postTask)
+  .get(matchUser, tasksController.getTasks)
+
+tasksRouter.route('/:id')
+  .get(matchUser, requiredTask, tasksController.getTask)
+  .patch(matchUser, requiredTask, requireTaskProps, tasksController.patchTask)
+  .delete(matchUser, requiredTask, tasksController.deleteTask)
 
 export { tasksRouter }
