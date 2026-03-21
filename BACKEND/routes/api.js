142cd9b036c8356c92880bb785630a29af75b895
Brandon Pérez <brandonperez@Brandons-Mac-mini.local>
2026-03-21 00:29:51 -0600

    refactor api router

--- BACKEND/routes/api.js
+++ BACKEND/routes/api.js
@@ -1,33 +1,23 @@
-import express from 'express'
-import path from 'path'
+import { Router, static as staticFile } from 'express'
+import  path  from 'path'
 import { fileURLToPath } from 'url'
 
 import { usersRouter } from './users.js'
 import { tagsRouter } from './tags.js'
 import { tasksRouter } from './tasks.js'
 
+import { requireSession, requireAuthField } from './middlewares.js'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const apiRouter = express.Router()
+const apiRouter = Router()
 
 const VIEWS_PATH = path.join(__dirname, '/../../FRONTEND/views')
 
-// Middlewares
-function authRedirect(req, res, next) {
-  if (!req.get('Auth')) return res.redirect('/login.html')
-
-  next()
-}
-
-function auth401(req, res, next) {
-  if (!req.get('Auth')) return res.sendStatus(401)
-
-  next()
-}
 // Archivos estaticos (estilos y imagenes)
-apiRouter.use('/styles', express.static(path.join(VIEWS_PATH, 'styles')))
-apiRouter.use('/assets', express.static(path.join(VIEWS_PATH, 'assets')))
+apiRouter.use('/styles', staticFile(path.join(VIEWS_PATH, 'styles')))
+apiRouter.use('/assets', staticFile(path.join(VIEWS_PATH, 'assets')))
 
 apiRouter.use('/users', usersRouter)
 apiRouter.use('/tags', tagsRouter)
@@ -37,17 +27,17 @@ apiRouter.get('/login.html', (req, res) => {
   res.sendFile(path.join(VIEWS_PATH, 'login.html'))
 })
 
-apiRouter.get('/', authRedirect, (req, res) => {
-  if (res.statusCode == 401) res.redirect('/login.html')
+apiRouter.get('/', requireSession, (req, res) => {
+  if (res.statusCode === 401) res.redirect('/login.html')
   else res.sendFile(path.join(VIEWS_PATH, 'home.html'))
 })
 
-apiRouter.get('/tasks.html', auth401, (req, res) => {
-  res.sendFile(path.join(__dirname, '/../../FRONTEND/views/tasks.html'))
+apiRouter.get('/tasks.html', requireAuthField, (req, res) => {
+  res.sendFile(path.join(VIEWS_PATH, 'tasks.html'))
 })
 
-apiRouter.get('/home.html', auth401, (req, res) => {
-  res.sendFile(path.join(__dirname, '/../../FRONTEND/views/home.html'))
+apiRouter.get('/home.html', requireAuthField, (req, res) => {
+  res.sendFile(path.join(VIEWS_PATH, 'home.html'))
 })
 
 export { apiRouter }
