b93987a22e735503ce880b563f30d7a4c34f6939
Brandon Pérez <brandonperez@Brandons-Mac-mini.local>
2026-03-24 16:36:03 -0600

    created the tag router with all validation in middlewares and controller

--- BACKEND/routes/tags.js
+++ BACKEND/routes/tags.js
@@ -1,7 +1,21 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { Router } from 'express'
 
-const tagsRouter = express.Router
+import { users, tags } from '../database/storage.js'
+
+import { matchUserPassword, requireTag, requireTagProps } from './middlewares.js'
+import * as tagsController from '../controllers/tags_controller.js'
+
+const tagsRouter = Router()
+const matchUser = matchUserPassword(users)
+const requiredTag = requireTag(tags)
+
+tagsRouter.route('/')
+  .post(matchUser, tagsController.postTag)
+  .get(matchUser, tagsController.getTags)
+
+tagsRouter.route('/:id')
+  .get(matchUser, requiredTag, tagsController.getTag)
+  .patch(matchUser, requiredTag, requireTagProps, tagsController.patchTag)
+  .delete(matchUser, requiredTag, tagsController.deleteTag)
 
 export { tagsRouter }
